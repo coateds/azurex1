ebe5e6f89ad8efeca64d64d5f22a2658de175f0a
Dave Coate <coateds@outlook.com>
2017-09-22 11:39:43 -0700

    Some Chocolatey and other SW installation resources

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,26 @@
 # Recipe:: default
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
+
+include_recipe 'chocolatey::default'
+
+chocolatey_package 'visualstudiocode'
+
+chocolatey_package 'git' do
+  options '--params /GitAndUnixToolsOnPath'
+end
+
+cookbook_file 'Win8.1AndW2K12R2-KB3191564-x64.msu' do
+	source 'Win8.1AndW2K12R2-KB3191564-x64.msu'
+end
+
+reboot 'Restart Computer' do
+  action :nothing
+end
+
+msu_package 'Install Windows WMF5.1 KB3191564' do
+  source 'Win8.1AndW2K12R2-KB3191564-x64.msu'
+  # action :remove
+  action :install
+  notifies :reboot_now, 'reboot[Restart Computer]', :immediately
+end
--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -7,12 +7,22 @@
 require 'spec_helper'
 
 describe 'azurex1::default' do
-  context 'When all attributes are default, on an Ubuntu 16.04' do
+  context 'When all attributes are default, on a Windows 2012R2' do
     let(:chef_run) do
       # for a complete list of available platforms and versions see:
       # https://github.com/customink/fauxhai/blob/master/PLATFORMS.md
-      runner = ChefSpec::ServerRunner.new(platform: 'ubuntu', version: '16.04')
+      runner = ChefSpec::ServerRunner.new(platform: 'windows', version: '2012R2')
       runner.converge(described_recipe)
+    end
+
+    it 'installs a package' do
+      expect(chef_run).to install_chocolatey_package('visualstudiocode')
+    end
+
+    it 'installs a package with options' do
+      expect(chef_run).to install_chocolatey_package('git').with(
+        options: '--params /GitAndUnixToolsOnPath'
+      )
     end
 
     it 'converges successfully' do
